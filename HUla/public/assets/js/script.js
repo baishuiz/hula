31d8ddcaae5d25a42e10abd7122459e23d00dd07
xmf薛梦飞 <mfxue@ctrip.com>
2015-07-08 11:56:36 +0800

    update service list

--- HUla/public/assets/js/script.js
+++ HUla/public/assets/js/script.js
@@ -1,5 +1,23 @@
 
 (function () {
+    var UI = (function () {
+        var $loading = $('#js-mask');
+        var $toast = $('#js-toast');
+        return {
+            showLoading: function() {
+                $loading.show();
+            },
+            hideLoading: function () {
+                $loading.hide();
+            },
+            showError: function (text) {
+                $toast.addClass('alert-danger').text(text).show();
+                setTimeout(function () {
+                    $toast.removeClass('alert-danger').text('').hide();
+                }, 2000)
+            }
+        };
+    })();
 
     /*
     * Ajax
@@ -35,8 +53,28 @@
                 });
         }
 
+        var put = function (url, data, successFn, errorFn) {
+            $.ajax({
+                url: url,
+                type: 'PUT',
+                data: data
+            }).done(function (data) {
+                    data = successHandler(data);
+                    if (data.ack) {
+                        errorFn && errorFn(data);
+                    } else {
+                        successFn && successFn(data);
+                    }
+                })
+                .fail(function (error) {
+                    error = errorHandler(error);
+                    errorFn && errorFn(error);
+                });
+        }
+
         return {
-            get: get
+            get: get,
+            put: put
         };
     })();
 
@@ -44,11 +82,54 @@
     * Service Page
     */
     var serviceAction = function () {
-        Ajax.get('/restapi/service', function (data) {
-            data.services
-        }, function (error) {
+        $('.js-service-table').on('click', '.js-edit', function (e) {
+            e.preventDefault();
+            var $target = $(this);
+            var $root = $target.closest('tr');
 
-        })
+            $target.hide();
+            $root.find('input').removeAttr('disabled');
+            $root.find('.js-save, .js-cancel').show();
+            $root.find('.js-delete').hide();
+        });
+
+        // TODO
+        $('.js-service-table').on('click', '.js-save', function (e) {
+            e.preventDefault();
+            var $target = $(this);
+            var $root = $target.closest('tr');
+            var id = $root.attr('data-id');
+
+            UI.showLoading();
+            Ajax.put('/restapi/service/' + id, {
+                name: $root.find('.js-input-name').val(),
+                url: $root.find('.js-input-url').val()
+            }, function (data) {
+                UI.hideLoading();
+                $target.hide();
+                $root.find('input').each(function () {
+                    $(this).attr('data-value', $(this).val()).attr('disabled', 'disabled')
+                });
+                $root.find('.js-cancel').hide();
+                $root.find('.js-edit, .js-delete').show();
+            }, function (error) {
+                UI.hideLoading();
+                error.msg && UI.showError(error.msg);
+            })
+        });
+
+        $('.js-service-table').on('click', '.js-cancel', function (e) {
+            e.preventDefault();
+            var $target = $(this);
+            var $root = $target.closest('tr');
+
+            $target.hide();
+            $root.find('input').each(function () {
+                $(this).val($(this).attr('data-value')).attr('disabled', 'disabled')
+            });
+            $root.find('.js-save').hide();
+            $root.find('.js-edit, .js-delete').show();
+        });
     };
 
 
