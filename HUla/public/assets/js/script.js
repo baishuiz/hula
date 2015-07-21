ef9da6a4869cc4c68fa81c2e796160981d3b1c71
xmf薛梦飞 <mfxue@ctrip.com>
2015-07-21 17:07:24 +0800

    add cid

--- HUla/public/assets/js/script.js
+++ HUla/public/assets/js/script.js
@@ -331,6 +331,86 @@
     };
 
     /*
+    * Cookie
+    */
+    var Cookie = {
+
+        /**
+        * 写入cookie
+        * @param {String} key cookie 名称
+        * @param {String|Object} value 可以为字符串或者json对象
+        * @param {Date} [timeout=Session] 有效期
+        * @param {String} [path=/] 路径
+        * @param {String} [domain] 域名,默认为当前域名
+        * @param {boolean} [secure=false] 安全级别
+        */
+        set: function (key, value, timeout, path, domain, secure) {
+            value = _.isObject(value)?JSON.stringify(value):value;
+            document.cookie = key + "=" + escape(value) +
+                ((timeout) ? "; expires=" + timeout : "") +
+                ((path) ? "; path=" + path : "") +
+                ((domain) ? "; domain=" + domain : "") +
+                ((secure) ? "; secure" : "");
+        },
+        /**
+         * 返回cookien值
+         * @param {int} offset
+         * @returns {*} value
+         */
+        getCookieVal: function (offset) {
+            var endstr = document.cookie.indexOf(";", offset),result;
+            if (endstr == -1) {
+                endstr = document.cookie.length;
+            }
+            var str = unescape(document.cookie.substring(offset, endstr));
+            try{
+                result = JSON.parse(str);
+            }catch(e){
+                result = str;
+            }
+            return result;
+        },
+
+        /**
+         * 读取cookie值
+         * @param {String} key cookie名称
+         * @returns {String|Object} value cookie值
+         */
+        get: function (key) {
+            var cookie = document.cookie;
+            var arg = key + "=";
+            var alen = arg.length;
+            var clen = cookie.length;
+
+            var i = 0;
+            while (i < clen) {
+                var j = i + alen;
+                if (cookie.substring(i, j) == arg) {
+                    return this.getCookieVal(j);
+                }
+                i = cookie.indexOf(" ", i) + 1;
+                if (i === 0) {break;}
+            }
+            return null;
+        },
+
+        /**
+        * 值cookie为无效
+        * @param {String} key cookie name
+        * @param {String} [path] path
+        * @param {String} [domain] 域名
+        */
+        remove: function (key, path, domain) {
+            if (this.get(key)) {
+                document.cookie = key + "=" +
+                    ((path) ? "; path=" + path : "") +
+                    ((domain) ? "; domain=" + domain : "") +
+                    "; expires=Thu, 01-Jan-1970 00:00:01 GMT";
+            }
+        }
+    }
+
+    /*
     * UI
     */
     var UI = (function () {
@@ -742,14 +822,17 @@
             })
         });
 
-        // case editor
-        $casePage.find('li>.list-tree').each(function() {
-            var $target = $(this);
-            var index = $target.index($target.parent().children('.list-tree'));
-            if (index === 0) {
-                $target.find('.js-delete').hide();
-            }
-        });
+        var hideFirstDeleteBtn = function () {
+            // case editor
+            $casePage.find('li>.list-tree').each(function() {
+                var $target = $(this);
+                var index = $target.index($target.parent().children('.list-tree'));
+                if (index === 0) {
+                    $target.find('.js-delete').hide();
+                }
+            });
+        };
+        hideFirstDeleteBtn();
 
         //copy form template by melvin.ren
         var loop = function (ary, parentIsList, parentNode) {
@@ -824,6 +907,7 @@
           }
           var reqhtml = loop(con_req, false, requestObj);
           requestBox.html(reqhtml);
+          hideFirstDeleteBtn();
         });
 
         $casePage.on('blur', '#js_caseResString', function(e){
@@ -844,6 +928,7 @@
           }
           var reshtml = loop(con_res, false, responseObj);
           responseBox.html(reshtml);
+          hideFirstDeleteBtn();
         });
 
         // select all
@@ -911,9 +996,12 @@
                 var subEnv = JSON.parse($caseResultPage.find('.js-sub-env').val() || 'null');
                 var needSubEvn = JSON.parse($caseResultPage.find('.js-need-sub-env').val() || 'false');
                 var auth = JSON.parse($caseResultPage.find('.js-auth').val() || 'null');
-                var url = 'http://' + host + '/restapi/soa2/' + service.url + ( needSubEvn ? ('?subEnv=' + (subEnv || 'fat80')) : '');
+                var cid = Cookie.get('cid');
 
                 var sentReq = function (i) {
+                    var url = 'http://' + host + '/restapi/soa2/' + service.url + ( needSubEvn ? ('?subEnv=' + (subEnv || 'fat63')) : '');
+                    url += (needSubEvn ? '&' : '?') + '_fxpcqlniredt=' + cid;
+
                     i = i || 0;
                     var caseObj = cases[i];
                     var $caseElm = $cases.eq(i);
@@ -934,7 +1022,7 @@
                             contentType: 'json',
                             head: {
                                 auth: auth || null,
-                                cid: '',
+                                cid: cid,
                                 ctok: '',
                                 cver: '1.0',
                                 lang: '01',
@@ -970,7 +1058,33 @@
                     }
                 };
 
-                sentReq();
+                if (!cid) {
+                    var url = 'http://' + host + '/restapi/soa2/10290/createclientid?systemcode=09&createtype=3&contentType=json';
+
+                    UI.showLoading();
+                    Ajax.get(url, function (data) {
+                        UI.hideLoading();
+                        cid = data.ClientID;
+
+                        if (cid) {
+                            var now = new Date();
+                            var threeYearLater = new Date(now);
+                            threeYearLater.setFullYear(threeYearLater.getFullYear() + 3);
+
+                            Cookie.set('cid', cid, threeYearLater.toUTCString(), '/');
+                            sentReq();
+                        } else {
+                            UI.showError('创建uid失败');
+                        }
+                    }, function (error) {
+                        UI.hideLoading();
+                        UI.showError('创建uid失败');
+                        //sentReq();
+                    });
+                } else {
+                    sentReq();
+                }
+
             }
         })();
     };
